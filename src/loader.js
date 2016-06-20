4dff430f95688ef2a7adb6de283a5b5c4352d5fb
AsifAmin <asif_udr@yahoo.com>
2016-06-20 15:55:34 +1000

    added forms js

--- src/loader.js
+++ src/loader.js
@@ -8,7 +8,7 @@ qg.glue.data = qg.glue.data || {};
 qg.glue.loader = {
     init : function (callback) {
         if($(".qg-glue-charts").length > 0)  this.loadCharts(callback);
-        else if($(".qg-glue-forms").length > 0)  this.loadCharts(callback);
+        else if($(".qg-glue-forms").length > 0)  this.loadForms(callback);
         else this.commonLibs(callback);
     },
     commonLibs : function (callback) {
@@ -19,7 +19,7 @@ qg.glue.loader = {
         console.log('%c charts libraries has been loaded', 'background: #222; color: #bada55');
         $.getScript("https://rawgit.com/qld-gov-au/glue-module-loader/master/htdocs/assets/js/charts.js" , callback);
     },
-    loadforms : function (callback) {
+    loadForms : function (callback) {
         console.log('%c forms libraries has been loaded', 'background: #222; color: #bada55');
         $.getScript("https://rawgit.com/qld-gov-au/glue-module-loader/master/htdocs/assets/js/forms.js" , callback);
     }
