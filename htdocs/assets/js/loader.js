6e75baa77589ed37c700c499ba014303ac650b0b
matthewharding <matthew.harding@justice.qld.gov.au>
2016-07-29 12:32:02 +1000

    Update loader.js
    Changed URL references to Git file bridge (./?a=476897:)

--- htdocs/assets/js/loader.js
+++ htdocs/assets/js/loader.js
@@ -13,15 +13,15 @@ qg.glue.loader = {
     },
     commonLibs : function (callback) {
         console.log('%c loader could not find any specific classes so just the common libraries has been loaded ', 'background: #222; color: #bada55');
-        $.getScript("https://rawgit.com/qld-gov-au/glue-module-loader/master/htdocs/assets/js/common.js" , callback);
+        $.getScript("./?a=476897:htdocs/assets/js/common.js" , callback);
     },
     loadCharts : function (callback) {
         console.log('%c charts libraries has been loaded', 'background: #222; color: #bada55');
-        $.getScript("https://rawgit.com/qld-gov-au/glue-module-loader/master/htdocs/assets/js/charts.js" , callback);
+        $.getScript("./?a=476897:htdocs/assets/js/charts.js" , callback);
     },
     loadForms : function (callback) {
         console.log('%c forms libraries has been loaded', 'background: #222; color: #bada55');
-        $.getScript("https://rawgit.com/qld-gov-au/glue-module-loader/master/htdocs/assets/js/forms.js" , callback);
+        $.getScript("./?a=476897:htdocs/assets/js/forms.js" , callback);
     }
 };
 
